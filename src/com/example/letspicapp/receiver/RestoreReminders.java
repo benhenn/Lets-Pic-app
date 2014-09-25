0ed41efa77f1f962fbd4203824dbf0baf4495a93
tendlich <tendlich@mail.uni-mannheim.de>
2014-09-25 16:03:17 +0200

    without test code

--- src/com/example/letspicapp/receiver/RestoreReminders.java
+++ src/com/example/letspicapp/receiver/RestoreReminders.java
@@ -14,9 +14,10 @@ public class RestoreReminders extends Service{
 
 	 @Override
 	  public int onStartCommand(Intent intent, int flags, int startId) {
+		 
 		 ReminderHandler.getInstance().setAllRemindersAfterReboot(getBaseContext());
 
-		 //TEST
+		 /*//TEST
 		 Calendar c = Calendar.getInstance();
 		 c.setTimeInMillis(c.getTimeInMillis() + (1 * 60 * 100));
 		 Alarm alarm1 = new Alarm(c, "mnt/sdcard/Pictures/LetsPicApp/IMG_1.jpg", "IMG_1.jpg");
@@ -24,7 +25,7 @@ public class RestoreReminders extends Service{
 		 c.setTimeInMillis(c.getTimeInMillis() + (1 * 60 * 100));
 		 Alarm alarm2 = new Alarm(c, "mnt/sdcard/Pictures/LetsPicApp/IMG_2.jpg", "IMG_2.jpg");
 		 ReminderHandler.getInstance().scheduleAlarm(alarm2, getApplicationContext());
-		 
+		 */
 		 
 		 return Service.START_NOT_STICKY;
 	  }
